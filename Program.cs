1f37b6a15964946f5aaa09bb19c2c5974f9f99a2
AdiL18 <adi.l@comstar.co.il>
2020-02-09 11:35:52 +0200

    add for

--- Program.cs
+++ Program.cs
@@ -7,6 +7,13 @@ namespace git_test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! this is test");
+
+            var projectName = "x-project";
+            Console.WriteLine("start project:" + projectName);
+            for (int i = 0; i < 200; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
